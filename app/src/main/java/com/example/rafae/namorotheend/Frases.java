4c421c44df4cbadd98509f32b0e0e4fbe9e23b3a
rafae <rafael.filgue@gmail.com>
2018-08-09 22:20:52 -0300

    frases 1

--- app/src/main/java/com/example/rafae/namorotheend/Frases.java
+++ app/src/main/java/com/example/rafae/namorotheend/Frases.java
@@ -3,12 +3,29 @@ package com.example.rafae.namorotheend;
 public class Frases {
 
     String frase[] = {
-            "frase1",
-            "frase2",
-            "frase3",
-            "frase4",
-            "frase5",
-            "frase6"
+            "Sempre me apaixono depois que acaba a paixão. Sempre namoro quando acaba o namoro. Só assim sei amar.",
+            "Quem põe ponto final numa paixão com o ódio, ou ainda ama, ou não consegue deixar de sofrer.",
+            "Terminar um namoro nunca é fácil, é como chegar no final de um livro que poderia ter uma continuação maravilhosa, mas não tem.",
+            "Espero que todas as lágrimas que estou derramando, lavem de vez a minha alma das expectativas frustradas de ser feliz ao seu lado.",
+            "A gente acha que já superou, que já esqueceu. Mas tem horas que o coração aperta de novo, né?”",
+            "Não ame pela beleza, pois um dia ela acaba. Não ame por admiração, pois um dia você se decepciona. Ame apenas, pois o tempo nunca pode acabar com um amor sem explicação.",
+            "O amor não tem tempo para acontecer, só acontece uma vez e não acaba; se você acha que ele já aconteceu e lamenta por ter acabado é por que ele nem começou.",
+            "Amor não é aquilo que quando chega você torce para que nunca acabe. O nome disso é feriado.",
+            "Dói acabar um relacionamento, mas dói muito mais ver o amor enfraquecer, morrer e se transformar em algo ruim.",
+            "Nosso amor acabou, mas ficam as memórias para contar a história que jamais será esquecida.",
+            "Tudo acabou menos o amor que às vezes me acorda para realidade!",
+            "Nem sempre quando um relacionamento termina significa que a história de amor acabe também.",
+            "Muitas vezes o namoro acaba, mas o amor não.",
+            "Porque a vida sempre continua, o término do namoro não é o fim de mais nada!",
+            "Permanecer em um relacionamento apenas para não terminar um namoro fecha algumas portas que o levaria a um verdadeiro romance.",
+            "Quando um namoro acaba, nasce uma nova chance de encontrar a pessoa amada.",
+            "O fim de um namoro pode ser o princípio de uma nova felicidade.",
+            "Na superação do fim do namoro aproveite para se amar como nunca!",
+            "O nosso namoro acabou no dia em que meu amor próprio aumentou.",
+            "Não estava preparada para ver fugir da minha mão aquilo que eu julgava ser eterno no meu coração.",
+            "Final de relação não é só janela que fecha, mas também porta que abre!",
+            "frase6",
+
     };
 
 }
