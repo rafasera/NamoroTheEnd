fac641ac05ea5e3a06bc43aaef594eee2667502c
rafae <rafael.filgue@gmail.com>
2018-08-12 20:22:46 -0300

    Prate do codigo java OK, falta apenas a UX de usuario para a tela

--- app/src/main/java/com/example/rafae/namorotheend/Frases.java
+++ app/src/main/java/com/example/rafae/namorotheend/Frases.java
@@ -7,7 +7,7 @@ public class Frases {
             "Quem põe ponto final numa paixão com o ódio, ou ainda ama, ou não consegue deixar de sofrer.",
             "Terminar um namoro nunca é fácil, é como chegar no final de um livro que poderia ter uma continuação maravilhosa, mas não tem.",
             "Espero que todas as lágrimas que estou derramando, lavem de vez a minha alma das expectativas frustradas de ser feliz ao seu lado.",
-            "A gente acha que já superou, que já esqueceu. Mas tem horas que o coração aperta de novo, né?”",
+            "A gente acha que já superou, que já esqueceu. Mas tem horas que o coração aperta de novo, né?",
             "Não ame pela beleza, pois um dia ela acaba. Não ame por admiração, pois um dia você se decepciona. Ame apenas, pois o tempo nunca pode acabar com um amor sem explicação.",
             "O amor não tem tempo para acontecer, só acontece uma vez e não acaba; se você acha que ele já aconteceu e lamenta por ter acabado é por que ele nem começou.",
             "Amor não é aquilo que quando chega você torce para que nunca acabe. O nome disso é feriado.",
@@ -24,7 +24,64 @@ public class Frases {
             "O nosso namoro acabou no dia em que meu amor próprio aumentou.",
             "Não estava preparada para ver fugir da minha mão aquilo que eu julgava ser eterno no meu coração.",
             "Final de relação não é só janela que fecha, mas também porta que abre!",
-            "frase6",
+            "No futuro não sei, mas agora temos caminhos diferentes, mesmo que ainda moremos no coração um do outro.",
+            "É triste! Mas acabou, e agora só me resta ser feliz sem a pessoa que um dia amei perdidamente.",
+            "A Vida Segue.",
+            "Porque a vida segue. Mas o que foi bonito fica com toda a força. Mesmo que a gente tente apagar com outras coisas bonitas ou leves, certos momentos nem o tempo apaga. E a gente lembra. E já não dói mais. Mas dá saudade... Uma saudade que faz os olhos brilharem por alguns segundos e um sorriso escapar volta e meia, quando a cabeça insiste em trazer a tona, o que o coração vive tentando deixar pra trás.",
+            "É muito difícil ter que esquecer quem aprendemos a amar.",
+            "E mesmo sorrindo por ai...Cada um sabe a falta que o outro faz.",
+            "Quando se lembrar de mim, de nós, lembra que não foi por minha vontade acabar assim.",
+            "Foi amizade, foi gostar, foi paixão, foi amar, e depois? Depois foi embora.",
+            "Pior que o término de um relacionamento, é não entender os motivos que levaram a esse fim.",
+            "Depois que você termina um namoro longo,\n" +
+                    "você começa a selecionar demais, \n" +
+                    "não entrega seu coração \n" +
+                    "como um dia já entregou.\n" +
+                    "Procura erros em todo mundo.\n" +
+                    "ao invés de valorizar as qualidades.\n",
+            "No momento que ela decidiu a musica final do relacionamento, nada a fez voltar atrás afinal, a vida dela sempre por trilhada por músicas agora não seria diferente.",
+            "Pensei em centenas de coisas para te dizer quando me pediu pra voltar. Sim. Não. Talvez. Quem sabe. Podemos tentar. Mas, eis: minha resposta:\n" +
+                    "Partiu novo amor. Partiu novos planos. Partiu. Parti. Tchau!\n",
+            "Motivos para fim do namoro: incompatibilidade de maturidade.",
+            "Nossa como você tá magrinha, deve tá sofrendo com o fim do namoro né?\n" +
+                    "É, sair pra queimar calorias dançando e beijando outros caras, ao invés daqueles programinhas onde a gente só comia, tem sido bem sofrido mesmo.\n",
+            "O Jeito mais fácil de estragar tudo, é dizer a coisa certa, usando as palavras erradas",
+            "Se inviável é a correção, oportuno é o ponto final.",
+            "Todo relacionamento que não acaba com separação, acaba em morte. Tudo desmorona no final.",
+            "Final de relacionamento: Um lado, refaz a vida do jeito que quer, o outro, do jeito que pode.",
+            "O amor as vezes é uma piada mal contada, você cria expectativas e no final não tem graça.",
+            "Nunca deposite sua felicidade em coisas, relacionamentos e pessoas, pois as coisas quebram, os relacionamentos terminam e as pessoas se vão.",
+            "Relacionamentos quase nunca acontecem como queremos. Isso porque podemos escolher o que fazemos, mas não o que recebemos.",
+            "Estar apaixonado não é problema.\n" +
+                    "O problema é quando você se apaixona por alguém que está longe de merecer qualquer tipo de afeto.\n",
+            "Quase paixão, quase amor, quase namoro, quase término, quase melhores amigos, quase para sempre, quase feliz. Cansei de quase.",
+            "É mais conveniente iniciar uma amizade, que se acabe em namoro, do que iniciar um namoro que se acabe numa amizade.",
+            "Como eu termino meu namoro da melhor forma possível? \n" +
+                    "Chega para o seu namorado e fala: \"Amor, me desculpa, mas Deus mandou amar o próximo\"\n",
+            "Tudo que começa tem fim, mas têm coisas que o fim é melhor que o começo.",
+            "Eu não sinto mais tanto sua falta quanto sentia antes!",
+            "Logo você encontrará outra pessoa. Procure nos achados e perdidos… principalmente nos perdidos.",
+            "Eu realmente gostaria de me casar com você, mas esperar que a morte nos separe é tempo demais.",
+            "Nós temos religiões diferentes... Você pensa que é Deus e eu não concordo.",
+            "Frequentamos grupos diferentes e somos muito diferentes um do outro. No inicio, isso foi maravilhoso e emocionante, mas para continuar, notei que será um problema para nós dois, por isso vou terminar.",
+            "Sabes, tu és a pessoa certa na hora errada",
+            "É que não temos mais química. Nem química, nem física, nem geometria (não nos cruzamos mais), nem matemática (porque não dividimos mais nada), e nem português (porque não falamos a mesma língua).",
+            "Eu andei a pensar em nós e… acho que temos objetivos diferentes, sabe",
+            "Saiba que eu levei você a sério. E isso é o que mais me dói.",
+            "Terminar faz parte da vida. Faz parte da vida terminar coisas ruins, para outras coisas BOAS entrarem.",
+            "Preciso me encontrar\n" +
+                    "Sinto que ainda não estou pronto(a), preciso me encontrar primeiro.\n",
+            "Eu tenho transtorno do déficit de atenção e não consigo ficar interessado em você por tempo suficiente.",
+            "Antes que seja tarde, \n" +
+                    "É melhor terminarmos agora que ainda somos amigos. Porque se continuar assim, nós não vamos aguentar olhar sequer um para o outro.\n",
+            "Eu não te mereço. Você é demais para mim. Tenho certeza que vai encontrar alguém melhor do que eu, que te mereça.",
+            "Você ainda vai conhecer uma pessoa maravilhosa… e paciente… principalmente paciente.",
+            "Acredito em finais felizes. Tipo, acabou agora e eu estou super feliz! Um final feliz.",
+            "O amor é uma viagem, por isso agradeço a sua carona.",
+            "Nosso relacionamento é do tipo PLUFT, PLAFT ZUM: Não vai a lugar nenhum.",
+            "Certas coisas podem ter me feito sofrer, mas elas simplesmente precisavam acontecer pra me fazer crescer.",
+            "Ainda sou apaixonada, sim. Mas desisti de você antes que eu tivesse que desistir de mim.",
+            "Eu gosto de você, mas com você ou sem você, segue o baile.",
 
     };
 
--- app/src/main/java/com/example/rafae/namorotheend/MainActivity.java
+++ app/src/main/java/com/example/rafae/namorotheend/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.rafae.namorotheend;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,17 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // foi instanciado o metodo da classe Frases para trazer o nome das frases
-    Frases minhaFrase = new Frases();
 
-    public void alteraFrase(View view){
+    // foi instanciado o metodo da classe Frases para trazer o valor das frases
+    Frases minhaFrase = new Frases();
+    public String recebeFrase;
+
+    public void alteraFrase(View view) {
 
         // Random com limitador a partir da classe intanciada
         int numero = new Random().nextInt(minhaFrase.frase.length);
 
         // apresenta a frase
-        TextView suaFrase = findViewById(R.id.txtFrase);
+        TextView suaFrase = (TextView) findViewById(R.id.txtFrase);
         suaFrase.setText(minhaFrase.frase[numero]);
+        recebeFrase = (String) suaFrase.getText();
+    }
+
+    // Bloco para copiar a frase carregada.
+
+    private void copyFrase() {
+        // Gets a handle to the clipboard service.
+        ClipboardManager clipboard = (ClipboardManager)
+                getSystemService(Context.CLIPBOARD_SERVICE);
+
+        // Criar um novo clip text que recebe o valor do clipboard
+        ClipData clipData = ClipData.newPlainText("",recebeFrase);
+        // Passa para o serviço de clipboard
+        clipboard.setPrimaryClip(clipData);
+        // Mensagem que indica que o texto foi copiado
+        Toast.makeText(this, "Essa frase foi copiada: " + recebeFrase, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void fraseCopiar(View view){
+        copyFrase();
+
     }
 
 
